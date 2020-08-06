dc63a3e6c321d72c857c4fbc78be6c15a49fe6e5
Alexandr-K13 <sanni3680@mail.ru>
2020-08-07 00:49:47 +0400

    Initial commit

---
+++ Java_homework_4/src/geekbrains/Employee.java
@@ -0,0 +1,9 @@
+package geekbrains;
+//Создать класс "Сотрудник" с полями: ФИО, зарплата, возраст;
+
+public class Employee {
+    String fullname;
+    int Age;
+    int Sallary;
+
+}
---
+++ Java_homework_4/src/geekbrains/Main.java
@@ -0,0 +1,78 @@
+p
+
+//Создать класс "Сотрудник" с полями: ФИО, зарплата, возраст;
+
+public class Employee {
+	String fullname;
+	int Age;
+	int Sallary;
+
+
+//Конструктор класса должен заполнять эти поля при создании объекта;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee c = new Employee[];
+		c.fullname = getfullname;
+		c.age = getage;
+		c.sallary = getsallary;
+
+		//Вывести при помощи методов из пункта 3 ФИО и возраст.
+		System.out.println(getfullname);
+		System.out.println(getage);
+		System.out.println(getsallary);
+
+		//Внутри класса «Сотрудник» написать методы, которые возвращают значение каждого поля;
+		private  void getfullname() {
+		return "Vasiliy Borisovich Kashin";
+		private  int getage(){
+			return 25;
+			private  int getsallary(){
+			return 70;
+			}
+		}
+    }
+	}
+}
+//Создать массив из 5 сотрудников.
+// С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+int []ar= new int [4];
+int ar[0] = Employee 1;
+int ar[1] = Employee 2;
+int ar[2] = Employee 3;
+int ar[3] = Employee 4;
+int ar[4] = Employee 5;
+
+if ( ar =0; ar >40; +ar);
+system.out.println ( Сотрудники старше 40: +ar );
+
+public static void Employee 1{
+		String name = Vasiliy;
+	int age = 30;
+	int sallary = 50;
+}
+
+public static void Employee 2{
+		String name = Dmitry;
+		int age = 45;
+		int sallary = 70;
+}
+
+public static void Employee 3{
+		String name = Alexandr;
+		int age = 40;
+		int sallary = 65;
+		}
+
+public static void Employee 4{
+		String name = Vasiliy;
+		int age = 30;
+		int sallary = 50;
+		}
+public static void Employee 5{
+		String name = Vasiliy;
+		int age = 30;
+		int sallary = 50;
+		}
